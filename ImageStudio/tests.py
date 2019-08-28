b7af24e902c18a262dca459ff29d9a5cffd54414
magechaP <magechapeter@gmail.com>
2019-08-28 15:44:10 +0300

    create and run the tests for our various functionalities

--- ImageStudio/tests.py
+++ ImageStudio/tests.py
@@ -1,70 +1,69 @@
 from django.test import TestCase
-from .models import Image,Category,Location 
+from .models import Image, Category, Location
 
 # Create your tests here.
+
+
 class LocationTest(TestCase):
     def setUp(self):
-        self.ian=Location(location="Nairobi");
+        self.ian = Location(location="Nairobi")
 
     def test_instance(self):
-        self.assertTrue(isinstance(self.ian,Location))
+        self.assertTrue(isinstance(self.ian, Location))
 
-    
     def test_save_image(self):
         self.ian.save_location()
-        location=Location.objects.all()
-        self.assertTrue(len(location)>0)
+        location = Location.objects.all()
+        self.assertTrue(len(location) > 0)
 
     def test_delete(self):
         self.ian.delete_location()
-        location=Location.objects.all()
-        self.assertTrue(len(location)==0)
+        location = Location.objects.all()
+        self.assertTrue(len(location) == 0)
+
 
 class TestCategory(TestCase):
     def setUp(self):
-        self.category=Category(Category="love")
-
+        self.category = Category(Category="love")
 
     def test_instance(self):
-        self.assertTrue(isinstance(self.category,Category))
+        self.assertTrue(isinstance(self.category, Category))
 
     def test_save_category(self):
         self.category.save_category()
-        category=Category.objects.all()
-        self.assertTrue(len(category)>0)
+        category = Category.objects.all()
+        self.assertTrue(len(category) > 0)
 
 
 class ImageTest(TestCase):
-        def setUp(self):
+    def setUp(self):
 
-            self.ian=Location(location="Nairobi");
-            self.ian.save_location()
+        self.ian = Location(location="Nairobi")
+        self.ian.save_location()
 
-            self.new_categ=Category(Category="food")
-            self.new_categ.save_category()
+        self.new_categ = Category(Category="food")
+        self.new_categ.save_category()
 
+        self.new_image = Image(name="lizania", decription="Finger Lingering",
+                               image="", categ=self.new_categ, locate=self.ian)
+        self.new_image.save()
 
-            self.new_image=Image(name="lizania",decription="Finger Lingering",image="",categ=self.new_categ,locate=self.ian)
-            self.new_image.save()
+    def test_save_image(self):
+        self.new_image.save_image()
+        saved = Image.objects.all()
+        self.assertTrue(len(saved) > 0)
 
+    def test_delete_image(self):
+        self.new_image.delete_image()
+        saved = Image.objects.all()
+        self.assertTrue(len(saved) == 0)
 
-        def test_save_image(self):
-            self.new_image.save_image()
-            saved=Image.objects.all()
-            self.assertTrue(len(saved)>0)
+    def test_test_image_id(self):
+        self.new_image.get_id()
+        id = Image.objects.all()
+        self.assertTrue(len(id) > 0)
 
-        def test_delete_image(self):
-            self.new_image.delete_image()
-            saved=Image.objects.all()
-            self.assertTrue(len(saved)==0)
-
-        def test_test_image_id(self):
-            self.new_image.get_id()
-            id=Image.objects.all()
-            self.assertTrue(len(id)>0)
-
-
-        def TearDown(self):
-            Location.objects.all().delete()
-            Category.objects.all().delete()
-            Image.objects.all().delete()
+    def TearDown(self):
+        Location.objects.all().delete()
+        Category.objects.all().delete()
+        Image.objects.all().delete()
